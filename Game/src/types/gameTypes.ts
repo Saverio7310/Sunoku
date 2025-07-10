f8595a1892ab3e3ea1454b6b53cdac22d3f7b8a4
Saverio Perrone <saverio.perrone20@gmail.com>
2025-07-10 15:37:24 +0200

    add new types for Board creation and GameState management

--- Game/src/types/gameTypes.ts
+++ Game/src/types/gameTypes.ts
@@ -1,11 +1,29 @@
+export type GameState = 'idle' | 'starting' | 'playing' | 'level-advancing' | 'game-over';
+
 export type Card = {
     type: 'card',
     value: number,
     isPlayed: boolean
 };
+
 export type Counter = {
     type: 'counter',
     bomb_value: number,
     count_value: number
 };
+
 export type Cell = Card | Counter;
+
+export type Message = {
+    type: 'log' | 'warning' | 'error',
+    text: string
+};
+
+export type Difficulty = 'easy' | 'medium' | 'hard'
+
+export type LevelData = {
+    difficulty: Difficulty,
+    ZERO: number,
+    TWO: number,
+    THREE: number,
+}

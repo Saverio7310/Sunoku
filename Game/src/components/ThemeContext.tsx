f9f5e9ffc0a030c7c4ec17f6e00f99e3214fced5
Saverio Perrone <saverio.perrone20@gmail.com>
2025-07-16 21:36:17 +0200

    add ThemeContext and ThemeProvider to handle page theme (light / dark)

---
+++ Game/src/components/ThemeContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { type ThemeContextType, type Theme } from "../types/gameTypes";
+
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: 'dark',
+    switchTheme: () => {
+        console.warn('Missing Theme switch function');
+    }
+});
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+}
+
+export function ThemeProvider({children}: ThemeProviderProps) {
+    const [theme, setTheme] = useState<Theme>('dark');
+
+    const switchTheme = () => {
+        setTheme(previous => previous === 'dark' ? 'light' : 'dark');
+    }
+
+    return (
+        <ThemeContext value={{ theme, switchTheme }}>
+            {children}
+        </ThemeContext>
+    );
+}

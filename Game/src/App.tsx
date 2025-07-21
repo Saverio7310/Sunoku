a5a9afa997827b42d4e9c02d018c230d35ac0040
Saverio Perrone <saverio.perrone20@gmail.com>
2025-07-21 16:36:42 +0200

    move cases' code into separate functions

--- Game/src/App.tsx
+++ Game/src/App.tsx
@@ -28,47 +28,45 @@ const boardReducer = (board: Cell[][], action: BoardAction): Cell[][] => {
             return action.board;
         }
         case 'PLAY_CARD': {
-            return board.map((row, i) => {
-                return row.map((cell, j) => {
-                    if (cell.type === 'card' && cell.isPlayed === false && i === action.rowIndex && j === action.colIndex) {
-                        let newCell: Card = {
-                            ...cell,
-                            isPlayed: true,
-                            areCandidatesVisible: Array(4).fill(null).map(() => false)
-                        };
-                        return newCell;
-                    } return cell;
-                });
-            });
+            return flipCard(board, action.rowIndex, action.colIndex);
         }
         case 'SHOW_CANDIDATE': {
-            return board.map((row, i) => {
-                return row.map((cell, j) => {
-                    if (cell.type === 'card' && cell.isPlayed === false && i === action.rowIndex && j === action.colIndex) {
-                        const newCell: Card = { ...cell, areCandidatesVisible: [...cell.areCandidatesVisible] };
-                        newCell.areCandidatesVisible[action.candidateIndex] = !newCell.areCandidatesVisible[action.candidateIndex];
-                        return newCell;
-                    } else return cell;
-                });
-            });
+            return showCandidate(board, action.rowIndex, action.colIndex, action.candidateIndex);
         }
         case 'REVEAL_BOARD': {
-            return board.map(row => {
-                return row.map(cell => {
-                    if (cell.type === 'card' && cell.isPlayed === false) {
-                        let newCell: Card = {
-                            ...cell,
-                            isPlayed: true,
-                            areCandidatesVisible: Array(4).fill(null).map(() => false)
-                        };
-                        return newCell;
-                    } else return cell;
-                });
-            });
+            return flipCard(board);
         }
         default:
             return board;
     }
+}
+
+const flipCard = (board: Cell[][], rowIndex: number | null = null, colIndex: number | null = null): Cell[][] => {
+    return board.map((row, i) => {
+        return row.map((cell, j) => {
+            const isTargetCell = (rowIndex === null && colIndex === null) || (i === rowIndex && j === colIndex);
+            if (cell.type === 'card' && cell.isPlayed === false && isTargetCell) {
+                let newCell: Card = {
+                    ...cell,
+                    isPlayed: true,
+                    areCandidatesVisible: Array(4).fill(null).map(() => false)
+                };
+                return newCell;
+            } return cell;
+        });
+    });
+}
+
+const showCandidate = (board: Cell[][], rowIndex: number, colIndex: number, candidateIndex: number): Cell[][] => {
+    return board.map((row, i) => {
+        return row.map((cell, j) => {
+            if (cell.type === 'card' && cell.isPlayed === false && i === rowIndex && j === colIndex) {
+                const newCell: Card = { ...cell, areCandidatesVisible: [...cell.areCandidatesVisible] };
+                newCell.areCandidatesVisible[candidateIndex] = !newCell.areCandidatesVisible[candidateIndex];
+                return newCell;
+            } else return cell;
+        });
+    });
 }
 
 const getInitialBoard = (): Cell[][] => {

aabc68b24d847b9edd0bc6645982c4149d2fba1a
Saverio Perrone <saverio.perrone20@gmail.com>
2025-07-23 18:39:30 +0200

    add CandidatesContextMenuProvider

--- Game/src/main.tsx
+++ Game/src/main.tsx
@@ -4,11 +4,14 @@ import './styles/colors.css'
 import './styles/index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ThemeContext.tsx'
+import { CandidatesContextMenuProvider } from './components/CandidatesContextMenuContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider>
-            <App />
+            <CandidatesContextMenuProvider>
+                <App />
+            </CandidatesContextMenuProvider>
         </ThemeProvider>
     </StrictMode>,
 )
